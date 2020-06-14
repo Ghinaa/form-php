d9f5031cceaac8b5f3d65ff7795cf5b1a83de664
ghinaa <https://github.com/Ghinaa/Ghinaa_1800016110/wiki/Instalasi-Angular>
2020-06-14 09:33:59 -0700

    form-php

---
+++ biodata.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<title>Output</title>
+</head>
+
+<body>
+
+<BR>
+<b><center>PRAKTIKUM TEKNOLOGI WEB LANJUT C</center>
+<BR>
+
+<table border="1" align="center">
+<BR>
+
+<tr>
+     <td width="117"><b>NIM</td>
+     <td width="218"> <?php echo $_POST["NIM"] ?></td>
+</tr>
+<tr>
+     <td><b>NAMA</td>
+     <td><?php echo $_POST["NAMA"]?></td>
+</tr>
+<tr>
+     <td><b>Jenis Kelamin</td>
+     <td><?php echo $_POST["KELAMIN"]?></td>
+</tr>
+<tr>
+     <td><b>Program Studi</td>
+     <td><?php echo $_POST["PRODI"]?></td>
+</tr>
+
+<table><BR>
+<b><center>PROGRAM STUDI SISTEM INFORMASI</center>
+<b><center>FAKULTAS SAINS DAN TEKNOLOGI TERAPAN</center>
+<b><center>UNIVERSITAS AHMAD DAHLAN YOGYAKARTA</center>
+<b><center>2020</center>
+<BR></table>
+
+</table>
+</body>
+</html>
---
+++ mhs.php
@@ -0,0 +1,45 @@
+<html>
+<body>
+
+<BR> <BR>
+<b><center>PRAKTIKUM TEKNOLOGI WEB LANJUT C</center></b>
+<BR><BR>
+
+<table border="1" align="center">
+<tr>
+     <td width="117"><b>NIM</td>
+     <td width="218"> <?php echo $_POST["NIM"] ?></td>
+</tr>
+<tr>
+     <td><b>NAMA</td>
+     <td><?php echo $_POST["NAMA"]?></td>
+</tr>
+<tr>
+     <td><b>JENIS KELAMIN</td>
+     <td><?php echo $_POST["KELAMIN"]?></td>
+</tr>
+<tr>
+     <td><b>ALAMAT</td>
+     <td><?php echo $_POST["ALAMAT"]?></td>
+</tr>
+<tr>
+     <td><b>PROGRAM STUDI</td>
+     <td><?php echo $_POST["PRODI"]?></td>
+</tr>
+<tr>
+     <td><b>BAHASA PEMROGRAMAN YANG DIKUASAI</td>
+     <td><?php echo $_POST["PEMROGRAMAN"]?></td>
+     
+</tr>
+<tr>
+
+<table><BR>
+<b><center>PROGRAM STUDI SISTEM INFORMASI</center>
+<b><center>FAKULTAS SAINS DAN TEKNOLOGI TERAPAN</center>
+<b><center>UNIVERSITAS AHMAD DAHLAN YOGYAKARTA</center>
+<b><center>2020</center>
+<BR></table>
+
+</table>
+</body>
+</html>
